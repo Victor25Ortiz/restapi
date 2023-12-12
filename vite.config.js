f76727d2b09ad31e0c59d7ea293faebf589bf157
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-11 21:56:58 -0600

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-restapi-magic/',
+  base: '/restapi/',
   plugins: [vue()],
   resolve: {
     alias: {
